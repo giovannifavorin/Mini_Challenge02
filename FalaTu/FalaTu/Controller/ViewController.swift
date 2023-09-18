74e8dc87d24fbc06788f9cf2040cbc14dbda8219
GustavoHoreste <101297032+GustavoHoreste@users.noreply.github.com>
2023-09-18 15:15:06 -0300

    Add files via upload

---
+++ FalaTu/FalaTu/Controller/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  FalaTu
+//
+//  Created by Gustavo Horestee Santos Barros on 18/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        
+        title = "teste"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+
+}
+
