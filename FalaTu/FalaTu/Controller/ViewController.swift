dd1536defc3ffbe85eea020cf4688a3162a40695
RoxasTA <victorbr64g@gmail.com>
2023-09-28 14:25:12 -0300

    Core Data parte 1

--- FalaTu/FalaTu/Controller/ViewController.swift
+++ FalaTu/FalaTu/Controller/ViewController.swift
@@ -7,14 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PersonCell", for: indexPath)
+        let person = self.items![indexPath.row]
+        
+        cell.textLabel?.text = person.nome
+        
+        return cell
+    }
+    
+    
+    @IBOutlet weak var tableView: UITableView!
 
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    var items:[Perfil]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
         
+        tableView.dataSource = self
+        tableView.delegate = self
+        
         title = "teste"
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func fetchPerfils() {
+        do {
+            self.items = try context.fetch(Perfil.fetchRequest())
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+        catch{
+            
+        }
     }
 
 
