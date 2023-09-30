01d3247b83fff329fa0003421ce806a8c0629a6a
Giovanni Favorin de Melo <Giovannifavorim@gmail.com>
2023-09-30 17:02:18 -0300

    Experimento de arquitetura de projeto

---
+++ FalaTu/FalaTu/Model/ModelMenuData.swift
@@ -0,0 +1,14 @@
+//
+//  ModelMenuData.swift
+//  FalaTu
+//
+//  Created by Giovanni Favorin de Melo on 30/09/23.
+//
+
+import Foundation
+import UIKit
+
+struct MenuDataModel{
+    let image: UIImage
+    let text: String
+}
