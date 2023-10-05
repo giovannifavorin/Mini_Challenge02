8fbf8201801e8964b858e7f799146e36c03598e4
Giovanni Favorin de Melo <Giovannifavorim@gmail.com>
2023-10-05 17:00:12 -0300

    updates

--- FalaTu/FalaTu/Games/WordOfDay/End/DefeatMinigame01ViewController.swift
+++ FalaTu/FalaTu/Games/WordOfDay/End/DefeatMinigame01ViewController.swift
@@ -6,13 +6,13 @@
 //
 
 import UIKit
+import SnapKit
 
 class DefeatMinigame01ViewController: UIViewController {
-
+    
     private lazy var background: UIImageView = {
         let background = UIImageView()
         background.image = UIImage(named: "pattern")
-        background.translatesAutoresizingMaskIntoConstraints = false
         background.contentMode = .scaleToFill // Configurar contentMode
         return background
     }()
@@ -51,18 +51,16 @@ class DefeatMinigame01ViewController: UIViewController {
     }()
     
     private lazy var scoreBoardBackground: UIImageView = {
-       let image = UIImageView()
+        let image = UIImageView()
         image.image = UIImage(named: "DefeatScoreboard")
-        image.translatesAutoresizingMaskIntoConstraints = false
-        image.contentMode = .scaleAspectFill
+        image.contentMode = .scaleAspectFit
         return image
     }()
     
     private lazy var shareButton: UIButton = {
         let button = UIButton(type: .custom)
         button.setImage(UIImage(named: "button_share"), for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.contentMode = .scaleAspectFit
+        button.contentMode = .scaleAspectFill
         button.addTarget(self, action: #selector(didButton), for: .touchUpInside)
         button.tag = 1
         return button
@@ -71,8 +69,7 @@ class DefeatMinigame01ViewController: UIViewController {
     private lazy var backButton: UIButton = {
         let button = UIButton(type: .custom)
         button.setImage(UIImage(named: "button_backmenu"), for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.contentMode = .scaleAspectFit
+        button.contentMode = .scaleAspectFill
         button.addTarget(self, action: #selector(didButton), for: .touchUpInside)
         button.tag = 2
         return button
@@ -81,7 +78,6 @@ class DefeatMinigame01ViewController: UIViewController {
     // Crie uma stack view horizontal
     private lazy var buttonStackView: UIStackView = {
         let buttonStackView = UIStackView()
-        buttonStackView.translatesAutoresizingMaskIntoConstraints = false
         buttonStackView.axis = .horizontal
         buttonStackView.spacing = 20
         return buttonStackView
@@ -89,11 +85,17 @@ class DefeatMinigame01ViewController: UIViewController {
     
     private lazy var labelStackView: UIStackView = {
         let stackView = UIStackView()
-        stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
-        stackView.spacing = 30
+        stackView.spacing = 20
         return stackView
     }()
+    
+    //    private lazy var containerStackView: UIStackView = {
+    //        let container = UIStackView()
+    //        container.axis = .vertical
+    //        container.spacing = 30
+    //        return container
+    //    }()
     
     @objc private func didButton(_ sender: UIButton!) {
         switch sender.tag {
@@ -110,46 +112,100 @@ class DefeatMinigame01ViewController: UIViewController {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor(named: "backgroundColor")
-
+        
         // Adicione os botões à stack view
         buttonStackView.addArrangedSubview(shareButton)
         buttonStackView.addArrangedSubview(backButton)
+        let containerView = UIView()
+        containerView.backgroundColor = .red
+        
+        //        containerStackView.addArrangedSubview(scoreBoardBackground)
+        //        containerStackView.addArrangedSubview(buttonStackView)
+        
+        view.addSubview(background)
+        view.addSubview(scoreBoardBackground)
+        view.addSubview(buttonStackView)
         
         labelStackView.addArrangedSubview(lostLabelTitle)
         labelStackView.addArrangedSubview(lostLabelBody)
         
-        // Adicione a UIImageView à hierarquia de visualização
-        view.addSubview(background)
-        view.addSubview(scoreBoardBackground)
+        //        containerView.addSubview(scoreBoardBackground)
+        //        containerView.addSubview(buttonStackView)
+        
         scoreBoardBackground.addSubview(labelStackView)
-        scoreBoardBackground.addSubview(timeLabel)
-        view.addSubview(buttonStackView)
-//        view.addSubview(regionLabel)
+        
+        background.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        //        containerView.snp.makeConstraints { make in
+        //            make.edges.equalToSuperview()
+        //        }
+        
+        buttonStackView.snp.makeConstraints { make in
+            //            make.bottom.equalToSuperview()
+            make.height.equalTo(60)
+            make.top.equalTo(scoreBoardBackground.snp.bottom).offset(60)
+            make.left.right.equalToSuperview().inset(60)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        scoreBoardBackground.snp.makeConstraints { make in
+            //            make.bottom.equalToSuperview().offset(60)
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.right.left.equalToSuperview().inset(10)
+        }
+        
+        labelStackView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(60)
+            make.centerX.equalToSuperview()
+        }
+        
+        //        containerStackView.snp.makeConstraints { make in
+        //            make.center.equalToSuperview()
+        //            make.edges.equalToSuperview().inset(40)
+        //        }
+        
+        //        labelStackView.addArrangedSubview(lostLabelTitle)
+        //        labelStackView.addArrangedSubview(lostLabelBody)
+        
+        
+        // Adicione a UIImageView à hierarquia de visualização
+        //
+        //
+        //        scoreBoardBackground.addSubview(labelStackView)
+        
+        //
+        //        view.addSubview(regionLabel)
         
         // Configure as restrições para preencher a tela inteira
-        NSLayoutConstraint.activate([
-            background.topAnchor.constraint(equalTo: view.topAnchor),
-            background.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            background.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            background.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            
-            scoreBoardBackground.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            scoreBoardBackground.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 1),
-            scoreBoardBackground.bottomAnchor.constraint(equalTo: buttonStackView.topAnchor, constant: -20),
-            
-            labelStackView.topAnchor.constraint(equalTo: scoreBoardBackground.topAnchor, constant: 85),
-            labelStackView.centerXAnchor.constraint(equalTo: scoreBoardBackground.centerXAnchor),
-            
-            timeLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 291),
-            timeLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 54),
-            timeLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -223),
-            timeLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -250),
-
-            buttonStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
-            buttonStackView.centerXAnchor.constraint(equalTo: scoreBoardBackground.centerXAnchor),
-//            buttonStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -88),
-
-        ])
+        //        NSLayoutConstraint.activate([
+        //            background.topAnchor.constraint(equalTo: view.topAnchor),
+        //            background.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        //            background.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+        //            background.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        
+        //            scoreBoardBackground.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        //            scoreBoardBackground.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 1),
+        //            scoreBoardBackground.bottomAnchor.constraint(equalTo: buttonStackView.topAnchor, constant: -20),
+        
+        //            labelStackView.topAnchor.constraint(equalTo: scoreBoardBackground.topAnchor, constant: 85),
+        //            labelStackView.centerXAnchor.constraint(equalTo: scoreBoardBackground.centerXAnchor),
+        //
+        
+        //            labelStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        //            labelStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        //            timeLabel.widthAnchor.constraint(equalToConstant: 10)
+        //            timeLabel.topAnchor.constraint(equalTo: scoreBoardBackground.topAnchor, constant: 10),
+        //            timeLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 54),
+        //            timeLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -223),
+        //            timeLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -250),
+        //
+        //            buttonStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
+        //            buttonStackView.centerXAnchor.constraint(equalTo: scoreBoardBackground.centerXAnchor),
+        //            buttonStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -88),
+        
+        //        ])
     }
-
+    
 }
