6e661d42548ef38d9b9116b15f17215c34a123f5
Giovanni Favorin de Melo <Giovannifavorim@gmail.com>
2023-10-05 14:05:17 -0300

    constraints

--- FalaTu/FalaTu/Games/WordOfDay/End/DefeatMinigame01ViewController.swift
+++ FalaTu/FalaTu/Games/WordOfDay/End/DefeatMinigame01ViewController.swift
@@ -17,11 +17,44 @@ class DefeatMinigame01ViewController: UIViewController {
         return background
     }()
     
+    private lazy var timeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Tempo"
+        label.textAlignment = .center
+        label.adjustsFontForContentSizeCategory = true
+        return label
+    }()
+    
+    private lazy var regionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Região"
+        label.textAlignment = .center
+        label.adjustsFontForContentSizeCategory = true
+        return label
+    }()
+    
+    private lazy var lostLabelTitle: UILabel = {
+        let label = UILabel()
+        label.text = "Não foi dessa vez, mano...!"
+        label.textAlignment = .center
+        label.adjustsFontForContentSizeCategory = true
+        return label
+    }()
+    
+    private lazy var lostLabelBody: UILabel = {
+        let label = UILabel()
+        label.text = "Acabaram suas chances. \n Tente novamente amanhã!"
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.adjustsFontForContentSizeCategory = true
+        return label
+    }()
+    
     private lazy var scoreBoardBackground: UIImageView = {
        let image = UIImageView()
         image.image = UIImage(named: "DefeatScoreboard")
         image.translatesAutoresizingMaskIntoConstraints = false
-        image.contentMode = .scaleAspectFit
+        image.contentMode = .scaleAspectFill
         return image
     }()
     
@@ -50,10 +83,17 @@ class DefeatMinigame01ViewController: UIViewController {
         let buttonStackView = UIStackView()
         buttonStackView.translatesAutoresizingMaskIntoConstraints = false
         buttonStackView.axis = .horizontal
-        buttonStackView.spacing = 20 // Espaçamento entre os botões
+        buttonStackView.spacing = 20
         return buttonStackView
     }()
     
+    private lazy var labelStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 30
+        return stackView
+    }()
     
     @objc private func didButton(_ sender: UIButton!) {
         switch sender.tag {
@@ -75,10 +115,17 @@ class DefeatMinigame01ViewController: UIViewController {
         buttonStackView.addArrangedSubview(shareButton)
         buttonStackView.addArrangedSubview(backButton)
         
+        labelStackView.addArrangedSubview(lostLabelTitle)
+        labelStackView.addArrangedSubview(lostLabelBody)
+        
         // Adicione a UIImageView à hierarquia de visualização
         view.addSubview(background)
         view.addSubview(scoreBoardBackground)
+        scoreBoardBackground.addSubview(labelStackView)
+        scoreBoardBackground.addSubview(timeLabel)
         view.addSubview(buttonStackView)
+//        view.addSubview(regionLabel)
+        
         // Configure as restrições para preencher a tela inteira
         NSLayoutConstraint.activate([
             background.topAnchor.constraint(equalTo: view.topAnchor),
@@ -86,11 +133,22 @@ class DefeatMinigame01ViewController: UIViewController {
             background.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             background.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             
-            scoreBoardBackground.topAnchor.constraint(equalTo: view.topAnchor, constant: view.frame.height * 0.1),
             scoreBoardBackground.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            scoreBoardBackground.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 1),
+            scoreBoardBackground.bottomAnchor.constraint(equalTo: buttonStackView.topAnchor, constant: -20),
+            
+            labelStackView.topAnchor.constraint(equalTo: scoreBoardBackground.topAnchor, constant: 85),
+            labelStackView.centerXAnchor.constraint(equalTo: scoreBoardBackground.centerXAnchor),
+            
+            timeLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 291),
+            timeLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 54),
+            timeLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -223),
+            timeLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -250),
 
-            buttonStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            buttonStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -view.frame.height * 0.1),
+            buttonStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
+            buttonStackView.centerXAnchor.constraint(equalTo: scoreBoardBackground.centerXAnchor),
+//            buttonStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -88),
+
         ])
     }
 
